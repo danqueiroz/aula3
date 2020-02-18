f45dc204ec4c318f2a66108a1eb914e64f4839fc
Daniel <queiroz.c.daniel@gmail.com>
2020-02-18 16:32:45 -0300

    Exercício finalizado

---
+++ exercicio2.py
@@ -0,0 +1,4 @@
+lista = {'1':'domingo','2':'segunda','3':'terça','4':'quarta','5':'quinta','6':'sexta','7':'sábado'}
+num_dia = input("Digite um número de 1 a 7: ")
+
+print("O dia da semana é", lista.get(num_dia))
