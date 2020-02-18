a2755768e1b80ab207450654f0bce2e27acf3f1c
Daniel <queiroz.c.daniel@gmail.com>
2020-02-18 16:50:47 -0300

    Inserir validador de número abaixo de 1 e acima de 7

--- exercicio2.py
+++ exercicio2.py
@@ -1,4 +1,17 @@
-lista = {'1':'domingo','2':'segunda','3':'terça','4':'quarta','5':'quinta','6':'sexta','7':'sábado'}
-num_dia = input("Digite um número de 1 a 7: ")
+lista = {
+        1:'domingo',
+        2:'segunda',
+        3:'terça',
+        4:'quarta',
+        5:'quinta',
+        6:'sexta',
+        7:'sábado'}
 
-print("O dia da semana é", lista.get(num_dia))
+repetir = "Sim"
+
+num_dia = int(input("Digite um número de 1 a 7: "))
+
+if num_dia > 7 and num_dia:
+    repetir = input("O número não é válido!")
+else:
+    print("O dia da semana é", lista.get(num_dia))
